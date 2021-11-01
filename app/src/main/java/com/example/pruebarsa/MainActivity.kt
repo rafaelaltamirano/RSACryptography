829920bd2615045e35bc1e767c866458889de773
rafael altamirano <rafaelaltamirano_@outlook.com>
2021-11-01 17:19:25 -0300

    add

--- app/src/main/java/com/example/pruebarsa/MainActivity.kt
+++ app/src/main/java/com/example/pruebarsa/MainActivity.kt
@@ -2,34 +2,57 @@ package com.example.pruebarsa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import java.security.PublicKey
-import java.util.*
-import java.security.*
-import javax.crypto.*
 import android.util.Base64
+import android.util.Log
+import java.lang.Exception
+import java.security.KeyFactory
+import java.security.spec.KeySpec
+import java.security.spec.PKCS8EncodedKeySpec
 import java.security.spec.X509EncodedKeySpec
 
-private const val PUBLICKEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvRJ7mH/OUUk8yG8X4bLVz3yxVAQBwLqQxccSxwQylkjgG26/6epdVnyEJtz4rXmrPldACkKw2SpKncI2AlXmjntMn7TbmMuUnqCdMTv7o9v9RupK+rY/zSIPd3nQC6Kl+7ZSq470UoaXK5vIiV40crJK4nH0l4cODbM71ESY6M84+QlzOOC9+Oa8L6kT72AlZCtBGuFHgHOxE9Uk4qaxCmddfKQF2582ubSzhG9UBySphS7ArwhvlZCuL+VWY9leSsyXKQdGG1jjweblwiONPd3pLylEjAx1uKy9O6b/5mXyFEGv0hcwnLnY2vW9RKY/BN3p5KQH7x3sl7RyFvtb1QIDAQAB"
 
+private const val PUBLICKEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvRJ7mH/OUUk8yG8X4bLVz3yxVAQBwLqQxccSxwQylkjgG26/6epdVnyEJtz4rXmrPldACkKw2SpKncI2AlXmjntMn7TbmMuUnqCdMTv7o9v9RupK+rY/zSIPd3nQC6Kl+7ZSq470UoaXK5vIiV40crJK4nH0l4cODbM71ESY6M84+QlzOOC9+Oa8L6kT72AlZCtBGuFHgHOxE9Uk4qaxCmddfKQF2582ubSzhG9UBySphS7ArwhvlZCuL+VWY9leSsyXKQdGG1jjweblwiONPd3pLylEjAx1uKy9O6b/5mXyFEGv0hcwnLnY2vW9RKY/BN3p5KQH7x3sl7RyFvtb1QIDAQAB"
+private const val PRIVATEKEY = "MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCmct6jfeIFbBL0Qxq9t5joEqc4eOPKdUTKOrTpfiyad1kWmX5z/IeHFyGs9GCxibgrsQfCTswK/fbKgYu1EDY6c6vL6H03uo3CjuodhHJLLK/M1gYPMzzwEX1Em/9JnwUEw26dcECDS6wNtH6SBTeSpsRvdwZO4OiKTuNiSiy5YMeuQdOD3eu6AZ8kECB6DIXppYBfVsmnytmm4U9uTfD7rGTmqY25a8NFunmxcPuRb4A8XCre3E88RlN4w2tju0ckJxB30BDipeeRuA3zNhg58tC7TI5RvR8cGKtiEh6/RN01G11kP2feJ9UNiJ4mTWNWqYbKUObiaEonXtVGm5hlAgMBAAECggEAYdBlV6yVU1xkcxiwMUcgW1kC3ePk8UgiybjgMcrNCWUTfH1tW1nOhyunxYzC+fIVwc9B4QUPUXJXQkAIyrmhNI5fKfARDdvajLgRipEX3WeAVB595xbUO7v7YoNN7YS+nUL/0RNzRfUFQfSM4OCh/NN14o8OrPHslcKwS0WNrM3pSEFvSKAPUeIb+45UBB7zh/oU6/QEecER3zwgA+PSWvJzsy20vLE4Ejehig9pkSQJC1P1SxvDBCFHlPZ6B5x1FBQw9iTVbhgdOA5NMdL2bTq9rSZmbxtirSRvQwktHzxLdwOYYDIIQ8RhqSvs2PDBcm/gJ8DPgilb5T72M0ZlrQKBgQDQ9poFq5dH0AnCTYQ3FfWr4uACKl0RWpzHuM9BFj0y3xA//sRsZ5YQ50y263XStssVa9vIgCyHVCnG4VMMYL/tfrKSAe/+g7TbqqgcMrNbuCzjhOy+MWRaqQHH5b+PMCOfKnR+7mM9oA/Jn9hpO/AoJqBmB8tQ2dlF0E06aCVAIwKBgQDL6mNZPasqMvvbw3KEFFZmevPAOcuGvf9nN4p/+5rw4/pWBGHAK2h6zzgFReesOUsE4qQ1QWmUeNtgUEZO7NEwjOKz5qURd6N8aQ5oPhLPiv+LWrvCLxrCCjDQ+Gav6hskbaIsbPoN0iP//DEUpr054+JfFDVVdPy1BdeSoACJ1wKBgHm2ud4FpXdJwPVxK0reupsk6e+49z6tddaLWgUCTCE+dnugDI9MAsfTwgZlvdo/aKkQixMNPWKR7keOZU43aHOQMbkD8eNgmc69Z3ANRBVvgI4BldfucxKvkH773okrtAP4uaRZfGqfubJqLaaWJ/trDFCBx7ZPPdQBUYpssQ3bAoGAdj6le5NCFQdZetc8KZDthSGZBQe+mJTGbKENZCePck6Q1J5X3KqPPF+7GXuu2vSiJuQR2nvi8GCgvZnvG0dCCsg3kaCWaamZmMQGwbX1DHqKstFbfT/5YTmtkw/n4+x57sbQOri6y5bmn0MrZtFFezNFCmNahgJ/o60Ok4zKoyMCgYAmKAFAdizh+O6tBh6qq4if6D0T5pROTdfYCduRzK852ZWJev9q06O27G8DWyYSn8G/DiEfKUVqjxmsnveggfRjSYhCCnE2hI/x7BXzF3b85qemYBMaoxMgNJb1cc4uX6tvq2CK/z4HksEsXK8yKSoqM+nJ/7SPAd2mduUlD6RDyA=="
+
+private const val publicKeyFull = "-----BEGIN PUBLIC KEY----\n$PUBLICKEY\n-----END PUBLIC KEY-----";
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        encode(PUBLICKEY,"yopmail@yopmail.com")
+        val rsa = RSA()
+
+        try {
+
+            val mensajeEncriptado: String = rsa.encrypt("yopmail@yopmail.com",  getPublicKey(PUBLICKEY))
+            Log.d("TAG1", "mensaje encriptado -> $mensajeEncriptado")
+            val mensajeDesencirptado: String = rsa.descrypt(mensajeEncriptado,getPrivateKey(PRIVATEKEY))
+            Log.d("TAG1", "mensaje desencritpatado -> $mensajeDesencirptado")
+        } catch (e: Exception) {
+            Log.d("TAG1", e.toString())
+        }
     }
 }
 
-fun encode(publicKey: String, textDecrypt: String): String {
-    val publicKeyFull = "-----BEGIN PUBLIC KEY----\n" + publicKey + "\n-----END PUBLIC KEY-----";
+fun getPublicKey(publicKey: String): KeySpec {
+    val publicKeyFull = publicKey
     val publicBytes = Base64.decode(publicKeyFull, Base64.DEFAULT)
     val keySpec = X509EncodedKeySpec(publicBytes)
-    val keyFactory: KeyFactory = KeyFactory.getInstance("RSA")
-    val pubKey: PublicKey = keyFactory.generatePublic(keySpec)
-    val cipher: Cipher = Cipher.getInstance("RSA/ECB/PKCS1PADDING") //or try with "RSA"
-    cipher.init(Cipher.ENCRYPT_MODE, pubKey)
-    val encrypted = cipher.doFinal(textDecrypt.toByteArray())
-    val encoded = Base64.encodeToString(encrypted, Base64.DEFAULT)
-    return encoded
+
+//    val keyFactory: KeyFactory = KeyFactory.getInstance("RSA")
+//    val pubKey: PublicKey = keyFactory.generatePublic(keySpec)
+//    val cipher: Cipher = Cipher.getInstance("RSA/ECB/PKCS1PADDING") //or try with "RSA"
+//    cipher.init(Cipher.ENCRYPT_MODE, pubKey)
+//    val encrypted = cipher.doFinal(textDecrypt.toByteArray())
+//    val encoded = Base64.encodeToString(encrypted, Base64.DEFAULT)
+   return keySpec
+}
+
+fun getPrivateKey(privateKey: String): PKCS8EncodedKeySpec  {
+    val publicKeyFull = privateKey
+    val privateBytes = Base64.decode(publicKeyFull, Base64.DEFAULT)
+    val keySpec = PKCS8EncodedKeySpec(privateBytes)
+
+    return keySpec
 }
 
 
--- app/src/main/java/com/example/pruebarsa/RSA.java
+++ app/src/main/java/com/example/pruebarsa/RSA.java
@@ -1,59 +1,117 @@
 package com.example.pruebarsa;
 
 import android.util.Base64;
+import android.util.Log;
 
-import java.security.Key;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
 import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
 import java.security.spec.KeySpec;
-import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 public class RSA {
-    public static String encryptRSAToString(String clearText, String publicKey) {
-        String encryptedBase64 = "";
-        try {
-            KeyFactory keyFac = KeyFactory.getInstance("RSA");
-            KeySpec keySpec = new X509EncodedKeySpec(Base64.decode(publicKey.trim().getBytes(), Base64.DEFAULT));
-            Key key = keyFac.generatePublic(keySpec);
 
-            // get an RSA cipher object and print the provider
-            final Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1");
-            // encrypt the plain text using the public key
-            cipher.init(Cipher.ENCRYPT_MODE, key);
 
-            byte[] encryptedBytes = cipher.doFinal(clearText.getBytes("UTF-8"));
-            encryptedBase64 = new String(Base64.encode(encryptedBytes, Base64.DEFAULT));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private KeyPairGenerator kpg;
+    private KeyPair kp;
+    private PublicKey publicKey;
+    private PrivateKey privateKey;
 
-        return encryptedBase64.replaceAll("(\r|\n)", "");
+    private String descryptedString;
+    private byte[] encrytedByte;
+    private byte[] descryptedByte;
+    private Cipher cipher;
+
+    private final static String CRYPTO_METHOD = "RSA";
+    private final static int CRYPTO_BITS = 2048;
+    private final static String OPCION_RSA = "RSA/ECB/PKCS1PADDING";
+    private final static String PUBLICKEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvRJ7mH/OUUk8yG8X4bLVz3yxVAQBwLqQxccSxwQylkjgG26/6epdVnyEJtz4rXmrPldACkKw2SpKncI2AlXmjntMn7TbmMuUnqCdMTv7o9v9RupK+rY/zSIPd3nQC6Kl+7ZSq470UoaXK5vIiV40crJK4nH0l4cODbM71ESY6M84+QlzOOC9+Oa8L6kT72AlZCtBGuFHgHOxE9Uk4qaxCmddfKQF2582ubSzhG9UBySphS7ArwhvlZCuL+VWY9leSsyXKQdGG1jjweblwiONPd3pLylEjAx1uKy9O6b/5mXyFEGv0hcwnLnY2vW9RKY/BN3p5KQH7x3sl7RyFvtb1QIDAQAB";
+    private final static String PRIVATEKEY = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC9EnuYf85RSTzIbxfhstXPfLFUBAHAupDFxxLHBDKWSOAbbr/p6l1WfIQm3Piteas+V0AKQrDZKkqdwjYCVeaOe0yftNuYy5SeoJ0xO/uj2/1G6kr6tj/NIg93edALoqX7tlKrjvRShpcrm8iJXjRyskricfSXhw4NszvURJjozzj5CXM44L345rwvqRPvYCVkK0Ea4UeAc7ET1STiprEKZ118pAXbnza5tLOEb1QHJKmFLsCvCG+VkK4v5VZj2V5KzJcpB0YbWOPB5uXCI4093ekvKUSMDHW4rL07pv/mZfIUQa/SFzCcudja9b1Epj8E3enkpAfvHeyXtHIW+1vVAgMBAAECggEALEpAGaEWu4PkcS9U8NHrtAcJoxytLHCHqUzQ4T3ie8HmfYab/jegVbolWtU4i6qMqrXVkpG8y6JUiFAORs3IjGflsyOyrUBDOEV3jSj38ZJ+6jXICqLWnz7zUJGAYrlfGG/FPdrm8M4Ga3i6qTMgJh4N3K3FFhnU+PCbe0X2a9J3BrjilsrYW8iSBr6yryWHQdztIQb/uKvZ+IXzxeviPCroGUTvRcNVC4NH36s5uR4GPWpRgGCKROedl2RUcaHoo/oOTrC0PkqgkBlhjsWxaJD/lHVXqMN6DgRyA5rF+2KAoHGQ4UMP6xeSXvEtgSZhwQH81MSgXw7tunNvmjVSoQKBgQD8D7uWZdQjHphFZ6wFOWAbD3Id+6gaPRT6honMjKfk89+CoQBz/2jye9CADSpXMHD5K88QDH2U23zZ7xJsFgpqpKlUDCihf56sZ+iE9Rsh1TNhtjbJHLUWWjLgIFqjB0QXchDgI8YTTg4mRUWN5oBBp/4jBWLdPmMWw2aPaX/iKwKBgQDABsmjd3G9Btj4/mefWwQRraITR/nMSOnO5EY0QKgVMSAhOZqfbBw0yUq7OIS7Ic/Y+sgplhoVu8e/dXxoAN";
+    private String message = "yolanda@yopmail.com";
+    private final static String publicKeyFull = "-----BEGIN PUBLIC KEY----$PUBLICKEY-----END PUBLIC KEY-----";
+
+
+    public void generateKayPair() throws Exception {
+        kpg = KeyPairGenerator.getInstance(CRYPTO_METHOD);
+        kpg.initialize(CRYPTO_BITS);
+        kp = kpg.generateKeyPair();
+        publicKey = kp.getPublic();
+        Log.d("TAG1", "public key -> " + publicKey);
+        privateKey = kp.getPrivate();
+        Log.d("TAG1", "private key -> " + privateKey);
     }
 
+    public String encrypt(String mensajeAEncriptar, KeySpec Key) throws Exception {
 
-    public static String decryptRSAToString(String encryptedBase64, String privateKey) {
+        cipher = Cipher.getInstance(OPCION_RSA);
 
-        String decryptedString = "";
-        try {
-            KeyFactory keyFac = KeyFactory.getInstance("RSA");
-            KeySpec keySpec = new PKCS8EncodedKeySpec(Base64.decode(privateKey.trim().getBytes(), Base64.DEFAULT));
-            Key key = keyFac.generatePrivate(keySpec);
+//        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Key.getBytes(StandardCharsets.UTF_8));
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PublicKey publicKey = keyFactory.generatePublic(Key);
 
-            // get an RSA cipher object and print the provider
-            final Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1");
-            // encrypt the plain text using the public key
-            cipher.init(Cipher.DECRYPT_MODE, key);
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        encrytedByte = cipher.doFinal(mensajeAEncriptar.getBytes());
+        return Base64.encodeToString(encrytedByte, Base64.DEFAULT);
+    }
 
-            byte[] encryptedBytes = Base64.decode(encryptedBase64, Base64.DEFAULT);
-            byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
-            decryptedString = new String(decryptedBytes);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public String descrypt(String result,KeySpec Key) throws Exception {
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PrivateKey privateKey = keyFactory.generatePrivate(Key);
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        descryptedByte = cipher.doFinal(Base64.decode(result, Base64.DEFAULT));
+        descryptedString = new String(descryptedByte);
+        return descryptedString;
 
-        return decryptedString;
+    }
+
+//    public String
+//    String publicKeyPemStr = getKey("public.pem");
+//
+//    publicKeyPemStr = publicKeyPemStr.replace("-----BEGIN PUBLIC KEY-----\n", "");
+//    publicKeyPemStr = publicKeyPemStr.replace("-----END PUBLIC KEY-----", "");
+//    byte[] encoded = Base64.decode(publicKeyPemStr, Base64.DEFAULT);
+//
+//    X509EncodedKeySpec  keySpec = new X509EncodedKeySpec(encoded);
+//    KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+//    PublicKey publicKey = keyFactory.generatePublic(keySpec);
+
+
+
+
+    public byte[] RSAEncrypt(final String plain) throws NoSuchAlgorithmException, NoSuchPaddingException,
+            InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        kpg = KeyPairGenerator.getInstance("RSA");
+        kpg.initialize(2048);
+        kp = kpg.genKeyPair();
+        publicKey = kp.getPublic();
+        privateKey = kp.getPrivate();
+
+        cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        encrytedByte = cipher.doFinal(plain.getBytes());
+
+        return encrytedByte;
+    }
+
+    public String RSADecrypt(final byte[] encryptedBytes) throws NoSuchAlgorithmException, NoSuchPaddingException,
+            InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+
+        cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        descryptedByte = cipher.doFinal(encryptedBytes);
+        String decrypted = new String(descryptedByte);
+        System.out.println("DDecrypted?????" + decrypted);
+        return decrypted;
     }
 
 }

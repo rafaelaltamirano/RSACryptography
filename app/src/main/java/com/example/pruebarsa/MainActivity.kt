ea5868741cead5b982af9786c880973bb32e55e9
rafael altamirano <rafaelaltamirano_@outlook.com>
2021-11-02 15:03:09 -0300

    agregado de claves ya desencriptadas en aes

--- app/src/main/java/com/example/pruebarsa/MainActivity.kt
+++ app/src/main/java/com/example/pruebarsa/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.example.pruebarsa
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Base64
 import android.util.Log
-import java.lang.Exception
-import java.security.KeyFactory
+import androidx.appcompat.app.AppCompatActivity
 import java.security.spec.KeySpec
 import java.security.spec.PKCS8EncodedKeySpec
 import java.security.spec.X509EncodedKeySpec
 
 
-private const val PUBLICKEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvRJ7mH/OUUk8yG8X4bLVz3yxVAQBwLqQxccSxwQylkjgG26/6epdVnyEJtz4rXmrPldACkKw2SpKncI2AlXmjntMn7TbmMuUnqCdMTv7o9v9RupK+rY/zSIPd3nQC6Kl+7ZSq470UoaXK5vIiV40crJK4nH0l4cODbM71ESY6M84+QlzOOC9+Oa8L6kT72AlZCtBGuFHgHOxE9Uk4qaxCmddfKQF2582ubSzhG9UBySphS7ArwhvlZCuL+VWY9leSsyXKQdGG1jjweblwiONPd3pLylEjAx1uKy9O6b/5mXyFEGv0hcwnLnY2vW9RKY/BN3p5KQH7x3sl7RyFvtb1QIDAQAB"
-private const val PRIVATEKEY = "MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCmct6jfeIFbBL0Qxq9t5joEqc4eOPKdUTKOrTpfiyad1kWmX5z/IeHFyGs9GCxibgrsQfCTswK/fbKgYu1EDY6c6vL6H03uo3CjuodhHJLLK/M1gYPMzzwEX1Em/9JnwUEw26dcECDS6wNtH6SBTeSpsRvdwZO4OiKTuNiSiy5YMeuQdOD3eu6AZ8kECB6DIXppYBfVsmnytmm4U9uTfD7rGTmqY25a8NFunmxcPuRb4A8XCre3E88RlN4w2tju0ckJxB30BDipeeRuA3zNhg58tC7TI5RvR8cGKtiEh6/RN01G11kP2feJ9UNiJ4mTWNWqYbKUObiaEonXtVGm5hlAgMBAAECggEAYdBlV6yVU1xkcxiwMUcgW1kC3ePk8UgiybjgMcrNCWUTfH1tW1nOhyunxYzC+fIVwc9B4QUPUXJXQkAIyrmhNI5fKfARDdvajLgRipEX3WeAVB595xbUO7v7YoNN7YS+nUL/0RNzRfUFQfSM4OCh/NN14o8OrPHslcKwS0WNrM3pSEFvSKAPUeIb+45UBB7zh/oU6/QEecER3zwgA+PSWvJzsy20vLE4Ejehig9pkSQJC1P1SxvDBCFHlPZ6B5x1FBQw9iTVbhgdOA5NMdL2bTq9rSZmbxtirSRvQwktHzxLdwOYYDIIQ8RhqSvs2PDBcm/gJ8DPgilb5T72M0ZlrQKBgQDQ9poFq5dH0AnCTYQ3FfWr4uACKl0RWpzHuM9BFj0y3xA//sRsZ5YQ50y263XStssVa9vIgCyHVCnG4VMMYL/tfrKSAe/+g7TbqqgcMrNbuCzjhOy+MWRaqQHH5b+PMCOfKnR+7mM9oA/Jn9hpO/AoJqBmB8tQ2dlF0E06aCVAIwKBgQDL6mNZPasqMvvbw3KEFFZmevPAOcuGvf9nN4p/+5rw4/pWBGHAK2h6zzgFReesOUsE4qQ1QWmUeNtgUEZO7NEwjOKz5qURd6N8aQ5oPhLPiv+LWrvCLxrCCjDQ+Gav6hskbaIsbPoN0iP//DEUpr054+JfFDVVdPy1BdeSoACJ1wKBgHm2ud4FpXdJwPVxK0reupsk6e+49z6tddaLWgUCTCE+dnugDI9MAsfTwgZlvdo/aKkQixMNPWKR7keOZU43aHOQMbkD8eNgmc69Z3ANRBVvgI4BldfucxKvkH773okrtAP4uaRZfGqfubJqLaaWJ/trDFCBx7ZPPdQBUYpssQ3bAoGAdj6le5NCFQdZetc8KZDthSGZBQe+mJTGbKENZCePck6Q1J5X3KqPPF+7GXuu2vSiJuQR2nvi8GCgvZnvG0dCCsg3kaCWaamZmMQGwbX1DHqKstFbfT/5YTmtkw/n4+x57sbQOri6y5bmn0MrZtFFezNFCmNahgJ/o60Ok4zKoyMCgYAmKAFAdizh+O6tBh6qq4if6D0T5pROTdfYCduRzK852ZWJev9q06O27G8DWyYSn8G/DiEfKUVqjxmsnveggfRjSYhCCnE2hI/x7BXzF3b85qemYBMaoxMgNJb1cc4uX6tvq2CK/z4HksEsXK8yKSoqM+nJ/7SPAd2mduUlD6RDyA=="
-private const val publicKeyFull = "-----BEGIN PUBLIC KEY----\n$PUBLICKEY\n-----END PUBLIC KEY-----";
-
+private const val PUBLICKEY =
+    "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA7KmWot7bz1kcvIpBN6XMgDPzcOBONLv9EvkWpFlf6tLAmgXUKzU57to8/Yd3MteLi71YTHYKuqvzECpr5hz+FADwbPlYOFYP6efyEiPZ/QbQu9Y0LJdCcrj6vsk2+3SU/eaDric+N+GuWkG9c2zbAF1rv1NFyQv4AFprhIOwyZ+TBelU4G+CNYfMJQ1ftxpTQzDgV9MBcRXDUXheWFstNGseNMSjWw7KPJbcUEWJ8ez33ilAwf1abe0bRZap8SJXBIQ+KKI4//yfk/QYAmBNzsgnzBYuHkAfMfO4oqdEYfvxEtAFJt0ntmMncFXKDBOkfL9KjlbozgpSZn4CJwn9jwIDAQAB"
+private const val PRIVATEKEY =
+    "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDsqZai3tvPWRy8ikE3pcyAM/Nw4E40u/0S+RakWV/q0sCaBdQrNTnu2jz9h3cy14uLvVhMdgq6q/MQKmvmHP4UAPBs+Vg4Vg/p5/ISI9n9BtC71jQsl0JyuPq+yTb7dJT95oOuJz434a5aQb1zbNsAXWu/U0XJC/gAWmuEg7DJn5MF6VTgb4I1h8wlDV+3GlNDMOBX0wFxFcNReF5YWy00ax40xKNbDso8ltxQRYnx7PfeKUDB/Vpt7RtFlqnxIlcEhD4oojj//J+T9BgCYE3OyCfMFi4eQB8x87iip0Rh+/ES0AUm3Se2YydwVcoME6R8v0qOVujOClJmfgInCf2PAgMBAAECggEBAJm3AP9irvS7VZ3kUr3ZM72lgJ41Ira/z32uShWPc2xrXOvtk0RJOwq3t1uYzEQ6mgm2fw6SsR9UJfi/BPweoqzNe1vcjH9y7r0niSRdIu5kqdkHJTY9PMyU91BKEys6KrfLLIZGOPRE4mD/SBUbhBd1llFbvfz6iCd6k1Er0/zwJ+YGC2KOpW1v8KdhPbVjP2c800N8/U96M3j1sa6Thu73BQrgdOjtxUTv6aLxXVemn65eqhLOUTYLSla+fOeWoecUPS4JxbPdjfwO6KHtAphMheNis+t7Wop22k02gEfufWslYx+zDAIRf76mbFMIuraCMLTvSmLE8Y0oYcakA+ECgYEA99kL4AAMEV+l6UggiCWUgiF0L3RRFbiRuAuZZDVFn1HwXtatxEt4EbjztAeniSE3aTUOjX1ferjQmGwQjPWwA3IJ4j9EX4ykXjA6ZwFiFlrZ5GQ7XStasBeO0HM2oMLC5U4VxTnHDon8KUg/OxavwLvc6U0sSnxRtdqb0kdYfv8CgYEA9HJblKnExzoOYKgrqg5twxFp39/kO9AkVKxlwpVqf+YpDXqG2qyhJ2i0kIg0J3rAKHU2h9Z3kHPA8eeobnXH+DVfpipWLt1AJusEncWeVN2e8uuTr6wGaKzYjXHoj/QTkHbxaLBWX0phGaPQaq9hXYMvzzoIqUOxw/3wvEJ1EXECgYAbLBeKMlcgXH0jCz5NztjuP16uiTEOkZKgO91QSCre4avZqe2ZFLEQOFe3iBNNDHA3v5Jd3r3mBXcRDt/tpE3swsbkoA0N8KxgiycL/fHUA5fZ20dK2qLxpVOJ9OerTk7xttLaMZBqPqR/niUYhE82cSIyDbzqWaDXsCTfM+U23wKBgFTldqr9/yqWHjIxleCc3EAYRyYKkzC2zDdgHINk8zTyUaWtvTIrLFkMg1GDDGH9vskOVF7+/E199i/NnfCnFUk9+Dc0kvP5wXMCwnqtDJ06zx5c9NDZNOybwyX0vqloQnc06AjM3WKA6ZJHE3ZJ7M3WZv9Yj3OB4DZKdpZbml7xAoGAXmJnAMyrnl7uVFF2Gxyg/suW7S668A0EDcQ/KKgSzEbrl2rmZ+16DYW8NWgqrpiSe0KtuBUcxH1q1ZnCT8ivxXFBleF8S6YUAKWbd1QGBciV+fosQzt3w0gSVG9FgmhFlWz/ci5BiVWuHft66GTHG/3pMZCJYxSVdl7jLm8AHAU="
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,29 +21,38 @@ class MainActivity : AppCompatActivity() {
         val rsa = RSA()
 
         try {
-            rsa.generateKayPair()
-            val mensajeEncriptado: String = rsa.encrypt("yopmail@yopmail.com",  getPublicKey(PUBLICKEY))
+//            rsa.generateKayPair() //Solo se utiliza para pruebas con claves autogeneradas
+            val mensajeEncriptado: String =
+                rsa.encrypt("yolanda@yopmail.com", getPublicKey(PUBLICKEY))
             Log.d("TAG1", "mensaje encriptado -> $mensajeEncriptado")
-            val mensajeDesencirptado: String = rsa.descrypt(mensajeEncriptado,getPrivateKey(PRIVATEKEY))
+            val mensajeDesencirptado: String =
+                rsa.descrypt(mensajeEncriptado, getPrivateKey(PRIVATEKEY))
             Log.d("TAG1", "mensaje desencritpatado -> $mensajeDesencirptado")
         } catch (e: Exception) {
             Log.d("TAG1", e.toString())
         }
     }
 }
-
+/**
+ * Esta función parsea la public key de formato string a formato KeySpec X509 que es la utilizada para la clave publica
+ * @param publicKey public key en formato string ya desencriptada en aes
+ * @return KeySpec
+ */
 fun getPublicKey(publicKey: String): KeySpec {
     val publicKeyFull = publicKey
     val publicBytes = Base64.decode(publicKeyFull, Base64.DEFAULT)
     val keySpec = X509EncodedKeySpec(publicBytes)
-   return keySpec
+    return keySpec
 }
-
-fun getPrivateKey(privateKey: String): PKCS8EncodedKeySpec  {
+/**
+ * Esta función parsea la private key de formato string a formato KeySpec PKCS8 que es la utilizada para la clave privada
+ * @param privateKey public key en formato string ya desencriptada en aes
+ * @return KeySpec
+ */
+fun getPrivateKey(privateKey: String): PKCS8EncodedKeySpec {
     val publicKeyFull = privateKey
     val privateBytes = Base64.decode(publicKeyFull, Base64.DEFAULT)
     val keySpec = PKCS8EncodedKeySpec(privateBytes)
-
     return keySpec
 }
 
--- app/src/main/java/com/example/pruebarsa/RSA.java
+++ app/src/main/java/com/example/pruebarsa/RSA.java
@@ -26,13 +26,10 @@ public class RSA {
     private final static String CRYPTO_METHOD = "RSA";
     private final static int CRYPTO_BITS = 2048;
     private final static String OPCION_RSA = "RSA/ECB/PKCS1PADDING";
-    private final static String PUBLICKEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvRJ7mH/OUUk8yG8X4bLVz3yxVAQBwLqQxccSxwQylkjgG26/6epdVnyEJtz4rXmrPldACkKw2SpKncI2AlXmjntMn7TbmMuUnqCdMTv7o9v9RupK+rY/zSIPd3nQC6Kl+7ZSq470UoaXK5vIiV40crJK4nH0l4cODbM71ESY6M84+QlzOOC9+Oa8L6kT72AlZCtBGuFHgHOxE9Uk4qaxCmddfKQF2582ubSzhG9UBySphS7ArwhvlZCuL+VWY9leSsyXKQdGG1jjweblwiONPd3pLylEjAx1uKy9O6b/5mXyFEGv0hcwnLnY2vW9RKY/BN3p5KQH7x3sl7RyFvtb1QIDAQAB";
-    private final static String PRIVATEKEY = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC9EnuYf85RSTzIbxfhstXPfLFUBAHAupDFxxLHBDKWSOAbbr/p6l1WfIQm3Piteas+V0AKQrDZKkqdwjYCVeaOe0yftNuYy5SeoJ0xO/uj2/1G6kr6tj/NIg93edALoqX7tlKrjvRShpcrm8iJXjRyskricfSXhw4NszvURJjozzj5CXM44L345rwvqRPvYCVkK0Ea4UeAc7ET1STiprEKZ118pAXbnza5tLOEb1QHJKmFLsCvCG+VkK4v5VZj2V5KzJcpB0YbWOPB5uXCI4093ekvKUSMDHW4rL07pv/mZfIUQa/SFzCcudja9b1Epj8E3enkpAfvHeyXtHIW+1vVAgMBAAECggEALEpAGaEWu4PkcS9U8NHrtAcJoxytLHCHqUzQ4T3ie8HmfYab/jegVbolWtU4i6qMqrXVkpG8y6JUiFAORs3IjGflsyOyrUBDOEV3jSj38ZJ+6jXICqLWnz7zUJGAYrlfGG/FPdrm8M4Ga3i6qTMgJh4N3K3FFhnU+PCbe0X2a9J3BrjilsrYW8iSBr6yryWHQdztIQb/uKvZ+IXzxeviPCroGUTvRcNVC4NH36s5uR4GPWpRgGCKROedl2RUcaHoo/oOTrC0PkqgkBlhjsWxaJD/lHVXqMN6DgRyA5rF+2KAoHGQ4UMP6xeSXvEtgSZhwQH81MSgXw7tunNvmjVSoQKBgQD8D7uWZdQjHphFZ6wFOWAbD3Id+6gaPRT6honMjKfk89+CoQBz/2jye9CADSpXMHD5K88QDH2U23zZ7xJsFgpqpKlUDCihf56sZ+iE9Rsh1TNhtjbJHLUWWjLgIFqjB0QXchDgI8YTTg4mRUWN5oBBp/4jBWLdPmMWw2aPaX/iKwKBgQDABsmjd3G9Btj4/mefWwQRraITR/nMSOnO5EY0QKgVMSAhOZqfbBw0yUq7OIS7Ic/Y+sgplhoVu8e/dXxoAN";
     private String message = "yolanda@yopmail.com";
-    private final static String publicKeyFull = "-----BEGIN PUBLIC KEY----$PUBLICKEY-----END PUBLIC KEY-----";
 
     /**
-     * Esta función genera una clave publica y privada aleatoria, se utiliza para pruebas ya que estas claves deben ser proporcionadas por backend
+     * Esta función genera una clave publica y privada aleatoria, se utiliza para pruebas ya que estas claves deben ser proporcionadas por API
      */
     public void generateKayPair() throws Exception {
         kpg = KeyPairGenerator.getInstance(CRYPTO_METHOD);
@@ -46,13 +43,14 @@ public class RSA {
     /**
      * Esta función códifica un mensaje.
      * @param mensajeAEncriptar mensaje a encriptar
+     * @param Key PublicKey en formato KeySpec X509
      * @return mensaje encriptado en base64
      */
     public String encrypt(String mensajeAEncriptar, KeySpec Key) throws Exception {
 
         cipher = Cipher.getInstance(OPCION_RSA);
-       // KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-      //  PublicKey publicKey = keyFactory.generatePublic(Key);
+       KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PublicKey publicKey = keyFactory.generatePublic(Key);
         cipher.init(Cipher.ENCRYPT_MODE, publicKey);
         encrytedByte = cipher.doFinal(mensajeAEncriptar.getBytes());
         return Base64.encodeToString(encrytedByte, Base64.DEFAULT);
@@ -60,11 +58,12 @@ public class RSA {
     /**
      * Esta función decódifica un mensaje.
      * @param  mensajeEncriptado a desencriptar
+     * @param Key PrivateKey en formato KeySpec PKCS8
      * @return mensaje encriptado
      */
     public String descrypt(String mensajeEncriptado, KeySpec Key) throws Exception {
-//        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-//        PrivateKey privateKey = keyFactory.generatePrivate(Key);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        PrivateKey privateKey = keyFactory.generatePrivate(Key);
         cipher.init(Cipher.DECRYPT_MODE, privateKey);
         descryptedByte = cipher.doFinal(Base64.decode(mensajeEncriptado, Base64.DEFAULT));
         descryptedString = new String(descryptedByte);
